f09dac3638907ece33007badd004ca4a72eb78d9
satvikaeda <satvika164.reddy@gmail.com>
2024-12-07 14:46:04 -0500

    quey context code added

--- streamApp.py
+++ streamApp.py
@@ -1,5 +1,10 @@
+
+import os
 import streamlit as st
-from langchain_openai import ChatOpenAI
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_chroma import Chroma
+import config
+from langchain_core.prompts import ChatPromptTemplate
 
 model = ChatOpenAI(model="gpt-4o", temperature=0, max_tokens=None, timeout=None, max_retries=2)
 
@@ -19,8 +24,21 @@ if query_text := st.chat_input("Ask your question here ... "):
 
 prediction = None
 
+def query():
+    embedding_model = OpenAIEmbeddings()
+    db = Chroma(persist_directory=config.CHROMA_DIR, embedding_function=embedding_model)
+    result = db.similarity_search_with_relevance_scores(query_text, k=5)
+    if len(result) == 0:
+        print("I don't know")
+        return
+    else:
+        return "\n ------ \n".join([doc.page_content for doc, score in result])
+
 if query_text:
-    prediction = model.predict(query_text)
+    context = query()
+    prompt_template = ChatPromptTemplate.from_template(config.PROMPT_TEMPLATE)
+    prompt = prompt_template.format(context=context, query=query_text)
+    prediction = model.predict(prompt)
 
 if prediction is not None:
     response = f"Bot: {prediction}"
